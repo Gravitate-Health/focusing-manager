a20f1b53186e85c389d8d15b72b2385e8aeed4b7
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-10-30 14:33:59 +0100

    update explanation

--- src/templates/explanationTemplate.ts
+++ src/templates/explanationTemplate.ts
@@ -1,56 +1,56 @@
 export const explanation = {
     "pregnancy-lens": {
-        en: "This section was highlighted because you are a female of 18+ years, and this section refers to potential issues with pregnancy or breastfeeding.",
-        es: "Esta sección fue resaltada porque usted es una mujer de 18+ años, y esta sección se refiere a posibles problemas con el embarazo o la lactancia.",
-        pt: "Esta seção foi destacada porque você é uma mulher com mais de 18 anos e esta seção refere-se a potenciais problemas relacionados à gravidez ou amamentação.",
-        da: "Denne sektion blev fremhævet, fordi du er en kvinde over 18 år, og denne sektion henviser til potentielle problemer med graviditet eller amning."
+        "en": "This section was highlighted because you are a female of 18+ years, and this section refers to potential issues with pregnancy or breastfeeding.",
+        "es": "Esta sección fue resaltada porque usted es una mujer de 18+ años, y esta sección se refiere a posibles problemas con el embarazo o la lactancia.",
+        "pt": "Esta seção foi destacada porque você é uma mulher com mais de 18 anos e esta seção refere-se a potenciais problemas relacionados à gravidez ou amamentação.",
+        "da": "Denne sektion blev fremhævet, fordi du er en kvinde over 18 år, og denne sektion henviser til potentielle problemer med graviditet eller amning."
     },
     "conditions-lens": {
-        en: "This section was highlighted because you have stated in your clinical summary the diagnostic of ",
-        es: "Esta sección fue resaltada porque usted ha declarado en su resumen clínico el diagnóstico de ",
-        pt: "Esta seção foi destacada porque você declarou no seu sumário clínico o diagnóstico de ",
-        da: "Denne sektion blev fremhævet, fordi du har angivet i dit kliniske resumé diagnosen "
+        "en": "This section was highlighted because you have stated in your clinical summary the diagnostic of ",
+        "es": "Esta sección fue resaltada porque usted ha declarado en su resumen clínico el diagnóstico de ",
+        "pt": "Esta seção foi destacada porque você declarou no seu sumário clínico o diagnóstico de ",
+        "da": "Denne sektion blev fremhævet, fordi du har angivet i dit kliniske resumé diagnosen "
     },
     "allergies-lens": {
-        en: [
+        "en": [
             "This section was highlighted because you have stated in your clinical summary an ",
             " to "
         ],
-        es: [
+        "es": [
             "Esta sección fue resaltada porque usted ha declarado en su resumen clínico una ",
             " a "
         ],
-        pt: [
+        "pt": [
             "Esta seção foi destacada porque você declarou no seu sumário clínico uma ",
             " a "
         ],
-        da: [
+        "da": [
             "Denne sektion blev fremhævet, fordi du har angivet i dit kliniske resumé en ",
             " over for "
         ]
     },
     "interaction-lens": {
-        en: [
+        "en": [
             "This section was highlighted because you have stated in your clinical summary that you are taking ",
             " which is counter-indicated with "
         ],
-        es: [
+        "es": [
             "Esta sección fue destacada porque has indicado en tu resumen clínico que estás tomando ",
             " que está contraindicado con "
         ],
-        pt: [
+        "pt": [
             "Esta seção foi destacada porque você indicou no seu sumário clínico que está tomando ",
             " que é contraindicado com "
         ],
-        da: [
+        "da": [
             "Denne sektion blev fremhævet, fordi du har angivet i dit kliniske resumé, at du tager ",
             " som er kontraindiceret med "
         ]
     },
     "default": {
-        en: "This section was highlighted because it is relevant to your health.",
-        es: "Esta sección fue resaltada porque es relevante para su salud.",
-        pt: "Esta seção foi destacada porque é relevante para a sua saúde.",
-        da: "Denne sektion blev fremhævet, fordi den er relevant for din sundhed."
+        "en": "This section was highlighted because it is relevant to your health.",
+        "es": "Esta sección fue resaltada porque es relevante para su salud.",
+        "pt": "Esta seção foi destacada porque é relevante para a sua saúde.",
+        "da": "Denne sektion blev fremhævet, fordi den er relevant for din sundhed."
     }
 }
