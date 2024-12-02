887e0c7b64dc9a69440a874b813c594ceea7c06a
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2024-12-02 14:34:56 +0100

    Fixed lens explanation conditional

--- src/controllers/lensesController.ts
+++ src/controllers/lensesController.ts
@@ -483,7 +483,7 @@ const focusProccess = async (req: Request, res: Response, epi: any, ips: any, pv
         
         leafletSectionList = await applyLensToSections(lense, leafletSectionList, lensFullName, lensApplied, responseMessage, epi, ips, completeLenses, res)
         
-        if (lensApplied.valueOf() != false && parsedLensesNames![i].lensName != "rmm-lens") {
+        if (lensApplied.valueOf() != false) {
             let epiExtensions = getExtensions(epi)
             epiExtensions.push({
                 "extension": [
