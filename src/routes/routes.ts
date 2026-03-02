a3c25b3085efb3587616199e1e8b444a4423eaa4
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2026-03-02 18:32:22 +0100

    refactor: split lensesControler and update routes to use new split

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as preprocessingController from "../controllers/preprocessingController";
 import * as lensesController from "../controllers/lensesController";
+import * as focusController from "../controllers/focusController";
 export const FocusingManagerRouter: Router = Router();
 
 FocusingManagerRouter.get("/preprocessing", preprocessingController.getPreprocessingServices);
 FocusingManagerRouter.get("/preprocessing/cache/stats", preprocessingController.getCacheStats);
 FocusingManagerRouter.post("/preprocessing/:epiId", preprocessingController.preprocess);
 FocusingManagerRouter.get("/lenses", lensesController.getLensesNames);
-FocusingManagerRouter.post("/focus/:epiId", lensesController.focus);
-FocusingManagerRouter.post("/focus", lensesController.focus);
+FocusingManagerRouter.post("/focus/:epiId", focusController.focus);
+FocusingManagerRouter.post("/focus", focusController.focus);
